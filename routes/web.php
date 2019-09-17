dfe44d2b6cf4a5f1f01a9059be717fca1e5c6978
hoangxiu3716 <54255638+hoangxiu3716@users.noreply.github.com>
2019-09-17 11:10:34 +0700

    hoang

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::get('/login', function () {
 Route::get('/signup', function () {
     return view('pages.signup');
 });
+Route::get('/trangchu', function () {
+    return view('pages.trangchu');
+});
